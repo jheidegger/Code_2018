4ea78a54793c4064a03720de63fc191161ef561e
Harrison McCarty <harrisonmccarty301@gmail.com>
2017-12-25 20:37:17 -0500

    Test

---
+++ git_Test/src/org/usfirst/frc/team3176/robot/otherClass.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team3176.robot;
+
+public class otherClass {
+	public void otherClass() {
+		boolean git = true; 
+	}
+	public String printBs() {
+		String test = "Hey";
+		return test; 
+	}
+}
