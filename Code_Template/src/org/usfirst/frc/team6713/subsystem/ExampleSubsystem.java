3a25b2175e6f238ec7ad5b1cbdddcad8552b38a8
jheidegger <jth3141@gmail.com>
2018-01-04 11:22:58 -0500

    Added a example subsystem

---
+++ Code_Template/src/org/usfirst/frc/team6713/subsystem/ExampleSubsystem.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team6713.subsystem;
+
+
+public class ExampleSubsystem extends Subsystem {
+
+	Loop mainLoop = new Loop()
+			{
+				@Override
+				public void onStart()
+				{
+					
+				}
+				@Override
+				public void onloop() {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void stop() {
+					// TODO Auto-generated method stub
+					
+				}
+			};
+	
+	
+	@Override
+	public void zeroAllSensors() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean checkSystem() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
