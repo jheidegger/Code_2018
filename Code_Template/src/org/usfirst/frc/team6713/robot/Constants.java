91db85d2285004c0db512bd75800c1c4e515e876
jheidegger <jth3141@gmail.com>
2018-01-04 10:42:05 -0500

    added code template to the master

---
+++ Code_Template/src/org/usfirst/frc/team6713/robot/Constants.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team6713.robot;
+/*
+ * All constants are stored in this file as static constant variables
+ */
+public class Constants {
+	//Robot Port Map NOTE: only change if the robot is rewired
+	
+	
+	
+	
+	//Subsystem constants
+	
+	
+	
+	
+
+}
