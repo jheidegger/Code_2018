9f1b154e3ebf50132bd58c1430720442d1975a40
Harrison McCarty <harrisonmccarty301@gmail.com>
2018-01-12 00:45:39 -0500

    Working on implementing swerve

--- ProjectCRAB/src/org/usfirst/frc/team6713/subsystem/Swervepod.java
+++ ProjectCRAB/src/org/usfirst/frc/team6713/subsystem/Swervepod.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team6713.subsystem;
 
 public class Swervepod extends Subsystem {
-
+	Swervepod(int id) {
+		
+	}
 	@Override
 	public void zeroAllSensors() {
 		// TODO Auto-generated method stub
