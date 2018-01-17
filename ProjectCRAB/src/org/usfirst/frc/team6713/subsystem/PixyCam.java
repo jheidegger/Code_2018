b0cad628677c30b4a73b7f516d8740c302d88c2a
Harrison McCarty <harrisonmccarty301@gmail.com>
2018-01-17 02:23:58 -0500

    New Pixy Code

--- ProjectCRAB/src/org/usfirst/frc/team6713/subsystem/PixyCam.java
+++ ProjectCRAB/src/org/usfirst/frc/team6713/subsystem/PixyCam.java
@@ -1,262 +1,98 @@
 package org.usfirst.frc.team6713.subsystem;
 
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import Vision.*;
 
-class Block
-{
-  // print block structure!
-  void print()
-  {
-    int i, j;
-    char buf[128], sig[6], d;
-	bool flag;
-    if (signature>PIXY_MAX_SIGNATURE) // color code! (CC)
-	{
-      // convert signature number to an octal string
-      for (i=12, j=0, flag=false; i>=0; i-=3) //assigns value to signature, x, y, width, height, and anlge
-      {
-        d = (signature>>i)&0x07;
-        if (d>0 && !flag)
-          flag = true;
-        if (flag)
-          sig[j++] = d + '0';
-      }
-      sig[j] = '\0';
-      printf("CC block! sig: %s (%d decimal) x: %d y: %d width: %d height: %d angle %d\n", sig, signature, x, y, width, height, angle);
-    }
-	else // regular block.  Note, angle is always zero, so no need to print
-      printf("sig: %d x: %d y: %d width: %d height: %d\n", signature, x, y, width, height); //prints out data to console instead of smartDashboard -> check on the side of the driver station, check +print and click view console
-    //Serial.print(buf);
-  }
-  uint16_t signature; //Identification number for your object - you could set it in the pixymon
-  uint16_t x; //0 - 320
-  uint16_t y; //0 - 200
-  uint16_t width;
-  uint16_t height;
-  uint16_t angle;
-  //uint16_t area;
-}
-
-public class PixyCam extends Subsystem {
-
-	enum BlockType
-	{
-		NORMAL_BLOCK, //normal color recognition
-		CC_BLOCK	  //color-code(chnage in angle) recognition
-	};
-	I2C i2c;
-	void PixyCam(){
-		BlockType blockType;// it is the enum on the top
-		bool  skipStart;	//skips to check 0xaa55, which is byte that tells pixy it is start of new frame
-		byte blockCount; //How many signatured objects are there?
-		byte blockArraySize; //not used in the code
-		Block blocks[100]; //array that stores blockCount array
-		
+public class PixyCam{
+	PixyPacket values;
+	I2C pixy;
+	PixyPacket[] packets;
+	PixyException pExc;
+	String print;
+	public PixyCam() {
+		pixy = new I2C(Port.kOnboard, 0x54);
+		packets = new PixyPacket[7];
+		pExc = new PixyException(print);
+		values = new PixyPacket();
 	}
-	bool getStart() //checks whether if it is start of the normal frame, CC frame, or the data is out of sync
-	{
-	  byte w, lastw;
-
-	  lastw = (byte) 0xffff;
-
-	  while(true)
-	  {
-	    w = getWord(); //This it the function right underneath
-	    if (w==0 && lastw==0)
-		{
-	      //delayMicroseconds(10);
-		  return false;
+	//This method parses raw data from the pixy into readable integers
+	public int cvt(byte upper, byte lower) {
+		return (((int)upper & 0xff) << 8) | ((int)lower & 0xff);
+	}
+	/*
+	public void pixyReset(){
+		pixy.reset();
+	}
+	*/
+	//This method gathers data, then parses that data, and assigns the ints to global variables
+	public PixyPacket readPacket(int Signature) throws PixyException { //The signature should be which number object in 
+		int Checksum;												   //pixymon you are trying to get data for
+		int Sig;
+		byte[] rawData = new byte[32];
+		try{
+			pixy.readOnly(rawData, 32);
+		} catch (RuntimeException e){
 		}
-	    else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD)
-		{
-	      blockType = NORMAL_BLOCK;
-	      return true;
+		if(rawData.length < 32){
+			System.out.println("byte array length is broken");
+			return null;
 		}
-	    else if (w==PIXY_START_WORD_CC && lastw==PIXY_START_WORD)
-		{
-	      blockType = CC_BLOCK;
-	      return true;
-		}
-		else if (w==PIXY_START_WORDX) //when byte recieved was 0x55aa instead of otherway around, the code syncs the byte
-		{
-		  printf("Pixy: reorder");
-		  getByte(); // resync
-		}
-		lastw = w;
-	  }
-	}
-
-byte getWord() //Getting two Bytes from Pixy (The full information)
-{
-	unsigned char buffer[2] = {0, 0};
-
-	i2c->ReadOnly(2, buffer);
-	//printf("\n c: %d ",buffer[0]);
-	printf("\n w: %d",buffer[1]<<8|buffer[0]);
-	return (buffer[1] << 8) | buffer[0]; //shift buffer[1] by 8 bits and add( | is bitwise or) buffer[0] to it
-}
-
-uint8_t getByte()//gets a byte
-{
-	unsigned char buffer[1] = {0};
-
-	i2c->ReadOnly(1, buffer);
-	return buffer[0];
-}
-uint8_t getBlocks(byte maxBlocks)
-{
-  blocks[0] = {0}; //resets the array - clears out data from previous reading
-  uint8_t i;
-  byte w, checksum, sum;
-  Block *block;
-  if (!skipStart) //when computer has not seen 0xaa55 (starting frame)
-  {
-	  if (getStart()==false)
-			return 0;
-  }
-  else
-	skipStart = false;
-
-  for(blockCount=0; blockCount<maxBlocks && blockCount<PIXY_MAXIMUM_ARRAYSIZE; blockCount++)
-  {
-    checksum = getWord();
-    SmartDashboard.PutNumber("checksum",checksum);
-    if (checksum==PIXY_START_WORD) // we've reached the beginning of the next frame - checking for 0xaa55
-    {
-      skipStart = true; //starts this function
-	  blockType = NORMAL_BLOCK;
-	  //Serial.println("skip");
-      return blockCount;
-    }
-	else if (checksum==PIXY_START_WORD_CC) //we've reacehd the beginning of the next frame - checking for 0xaa56
-	{
-	  skipStart = true;
-	  blockType = CC_BLOCK;
-	  return blockCount;
-	}
-    else if (checksum==0)
-      return blockCount;
-
-	//if (blockCount>blockArraySize)
-		//resize();
-
-	block = blocks + blockCount;
-
-    for (i=0, sum=0; i<sizeof(Block)/sizeof(byte); i++)
-    {
-      w = getWord();
-      sum += w; //sum = w + sum
-      *((byte *)block + i) = w; //converts block to interger value
-    }
-    if (checksum==sum)
-    {
-      blockCount++;
-    	SmartDashboard.PutNumber("pixy error", 0.0);
-    }
-    else
-      SmartDashboard.PutNumber("pixy error", 1.0);
-
-	w = getWord(); //when this is start of the frame
-	if (w==PIXY_START_WORD)
-	  blockType = NORMAL_BLOCK;
-	else if (w==PIXY_START_WORD_CC)
-	  blockType = CC_BLOCK;
-
-
-
-  }
-  return blockCount;
-}
-
-void visionLoop()
-{
-
-	Block target1;
-	Block target2;
-	int target1Index=0;
-	int target2Index=0;
-	float target1area=0;
-	float target2area=0;
-	float deltaX;
-	byte NumberOfTargets = (byte)getBlocks(20);
-	//blocks[0].print();
-	//blocks[1].print();
-
-	if(NumberOfTargets==0)
-	{
-		lostTargets++;
-	}
-	else
-	{
-		lostTargets=0;
-	}
-	if(lostTargets>5 || NumberOfTargets !=0)
-	{
-		for(int i =0; i<NumberOfTargets; i++)
-		{
-			if(i == 0)
-			{
-				target1area = blocks[i].width*blocks[i].height;
-				target1Index=0;
-			}
-			else
-			{
-				if(target1area<blocks[i].width*blocks[i].height)
-				{
-					target2area=target1area;
-					target2Index=target1Index;
-					target1Index=i;
-					target1area=blocks[i].width*blocks[i].height;
+		for (int i = 0; i <= 16; i++) {
+			int syncWord = cvt(rawData[i+1], rawData[i+0]); //Parse first 2 bytes
+			if (syncWord == 0xaa55) { //Check is first 2 bytes equal a "sync word", which indicates the start of a packet of valid data
+				syncWord = cvt(rawData[i+3], rawData[i+2]); //Parse the next 2 bytes
+				if (syncWord != 0xaa55){ //Shifts everything in the case that one syncword is sent
+					i -= 2;
 				}
-				else if(target2area<blocks[i].width*blocks[i].height)
-				{
-					target2area = blocks[i].width*blocks[i].height;
-					target2Index=i;
+				//This next block parses the rest of the data
+				Checksum = cvt(rawData[i+5], rawData[i+4]);
+				Sig = cvt(rawData[i+7], rawData[i+6]);
+				if(Sig <= 0 || Sig > packets.length){
+					break;
 				}
+				packets[Sig - 1] = new PixyPacket();
+				packets[Sig - 1].X = cvt(rawData[i+9], rawData[i+8]);
+				packets[Sig - 1].Y = cvt(rawData[i+11], rawData[i+10]);
+				packets[Sig - 1].Width = cvt(rawData[i+13], rawData[i+12]);
+				packets[Sig - 1].Height = cvt(rawData[i+15], rawData[i+14]);
+				//Checks whether the data is valid using the checksum *This if block should never be entered*
+				if (Checksum != Sig + packets[Sig - 1].X + packets[Sig - 1].Y + packets[Sig - 1].Width + packets[Sig - 1].Height) {
+					packets[Sig - 1] = null;
+					throw pExc;
+				}
+				break;
 			}
 		}
-//		if(blocks[target1Index].x<blocks[target2Index].x)
-//		{
-//			target1=blocks[target1Index];
-//			target2=blocks[target2Index];
-//		}
-//		if(blocks[target1Index].x<blocks[target2Index].x)
-//		{
-//			target1=blocks[target2Index];
-//			target2=blocks[target1Index];
-//		}
-		target1=blocks[target1Index];
-		target2=blocks[target2Index];
-		SmartDashboard.PutNumber("target1",target1.x);
-		SmartDashboard.PutNumber("target2",target2.x);
-		SmartDashboard.PutNumber("target1Index",target1Index);
-		SmartDashboard.PutNumber("target2Index",target2Index);
-		deltaX = target2.x-target1.x;
+		//Assigns our packet to a temp packet, then deletes data so that we dont return old data
+		PixyPacket pkt = packets[Signature - 1];
+		packets[Signature - 1] = null;
+		return pkt;
 	}
-
-	printf("blocks: ");printf("%d", NumberOfTargets);printf("\n"); //prints number of block to the console
-
-	SmartDashboard.PutNumber("xvalue1",blocks[0].x);
-	SmartDashboard.PutNumber("xvalue1",blocks[1].x);
-	SmartDashboard.PutNumber("deltaX",deltaX);
-	SmartDashboard.PutNumber("TargetsLost",lostTargets);
-	SmartDashboard.PutNumber("number of targets",NumberOfTargets);
-	 // prints x, y, width, and etc. to the console (the vairables in the block object)
-	printf("\n"); //new line(space)
-	SmartDashboard.PutBoolean("started",getStart());
-}
-
-	
-	@Override
-	public void zeroAllSensors() {
-		// TODO Auto-generated method stub
-
+	public int getX(){
+		return values.X;
 	}
-
-	@Override
-	public boolean checkSystem() {
-		// TODO Auto-generated method stub
-		return false;
+	public int getY(){
+		return values.Y;
 	}
-
+	public int getWidth(){
+		return values.Width;
+	}
+	public int getHeight(){
+		return values.Height;
+	}
+	public int getArea(){
+		return getWidth() * getHeight();
+	}
+	/* vvvvv Experimental Stuff vvvvv
+	int height = 12; //actual target height
+	int width = 24; //actual target width
+	int w = 320 - getX(); //320 is camera x resolution
+	int x = 5;
+	double d = Math.sqrt(41);
+	//gets the horizontal component of the distance
+	public double getDistance(int angle){
+		return width*320/(2*getWidth()*Math.tan(angle));
+	}
+	*/
 }
