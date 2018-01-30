9d462fc103dec0759e0142bc4d8f3d2fc0668cc8
Harrison McCarty <harrisonmccarty@rocketmail.com>
2018-01-30 18:43:00 -0500

    Add files via upload

--- ProjectCRABCAM/src/org/usfirst/frc/team6713/robot/Robot.java
+++ ProjectCRABCAM/src/org/usfirst/frc/team6713/robot/Robot.java
@@ -7,24 +7,44 @@
 
 package org.usfirst.frc.team6713.robot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class Robot extends IterativeRobot {
 	PixyCam cam = PixyCam.getInstance();
 	Drivetrain drive = Drivetrain.getInstance();
 	Joystick stick = new Joystick(0);
+	Timer timer = new Timer();
+	
 	@Override
 	public void robotInit() {
 	}
 
 	@Override
 	public void autonomousInit() {
+		//get side and set boolean
+		String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		timer.reset();
+		timer.start();
+		if(gameData.charAt(0) == 'L')
+		{
+			drive.autoLeft(timer);
+		} else {
+			drive.autoRight(timer);
+		}
+		
+		
 	}
 	
 	@Override
 	public void autonomousPeriodic() {
+		
 	}
-	
 	@Override
 	public void teleopPeriodic() {
 		if(stick.getRawButton(0)) {
