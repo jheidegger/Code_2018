03d1dc747614fc3f2f448fdc43a89aa36376e61c
Harrison McCarty <harrisonmccarty301@gmail.com>
2018-02-04 17:22:14 -0500

    Lots o' changes, more in a bit

--- ProjectCRAB/src/Robot/Robot.java
+++ ProjectCRAB/src/Robot/Robot.java
@@ -14,64 +14,47 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
 	
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
 	private Loop_Manager myLoops = Loop_Manager.getInstance();
-	private Drivetrain drive = Drivetrain.getInstance(); 
-	private PixyCam camera = PixyCam.getInstance();
-	private Joystick j;
-	private Joystick j2;
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	private Drivetrain driveTrain = Drivetrain.getInstance(); 
+	private PixyCam visionCam = PixyCam.getInstance();
+	private DriverStation velocityStick;
+	private DriverStation thetaStick;
+	private DriverStation buttonMonkey;
+
 	@Override
 	public void robotInit() {
-		camera.registerLoop();
-		drive.registerLoop();
+		velocityStick = new DriverStation(0, .08);
+		thetaStick = new DriverStation(1, .08);
+		buttonMonkey = new DriverStation(2, 0);
+		visionCam.registerLoop();
+		driveTrain.registerLoop();
 		myLoops.startLoops();
-		j = new Joystick(0);
-		j2 = new Joystick(1);
-		
-	}
-	@Override
-	public void autonomousInit() {
-		
-		
 	}
 	
-	/**
-	 * This function is called periodically during autonomous.
-	 */
+	@Override
+	public void autonomousInit() {
+	}
+	
 	@Override
 	public void autonomousPeriodic() {
 		myLoops.runLoops();
-		
 	}
-	/**
-	 * This function is called periodically during operator control.
-	 */
+
 	@Override
 	public void teleopPeriodic() {
 		myLoops.runLoops();
-		if(j.getRawButton(1)) {
-			camera.setSystemState(PixyCam.systemStates.TRACKING_CUBE);
-			drive.setSystemState(Drivetrain.systemStates.VISION_TRACK_TANK);
+		if(velocityStick.button(1)) {
+			visionCam.setSystemState(PixyCam.systemStates.TRACKING_CUBE);
+			driveTrain.setSystemState(Drivetrain.systemStates.VISION_TRACK_TANK);
 		}
-		drive.swerve(j.getY(), j.getX(), j.getX(), Drivetrain.driveCoords.FIELDCENTRIC, Drivetrain.driveType.PERCENTPOWER);
+		else {
+			driveTrain.swerve(velocityStick.vertical(), velocityStick.horizontal(), thetaStick.horizontal(), Drivetrain.driveCoords.FIELDCENTRIC, Drivetrain.driveType.PERCENTPOWER);
+		}
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
--- ProjectCRAB/src/Vision/PixyPacket.java
+++ ProjectCRAB/src/Vision/PixyPacket.java
@@ -1,8 +1,8 @@
 package Vision;
 
 public class PixyPacket {
-public int X;
-public int Y;
-public int Width;
-public int Height;
+	public int X;
+	public int Y;
+	public int Width;
+	public int Height;
 }
