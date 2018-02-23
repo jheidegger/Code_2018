87e0a0cbc2ee423e9739c97e4333ea14ca712854
Harrison McCarty <222008740@BROWNSBURG.K12.IN.US>
2018-02-23 11:21:41 -0500

    PathFollower

--- ProjectCRAB/src/Auton/PathFollower.java
+++ ProjectCRAB/src/Auton/PathFollower.java
@@ -1,8 +1,44 @@
 package Auton;
 
+import Util.PIDLoop;
 import edu.wpi.first.wpilibj.Timer;
 
 public class PathFollower {
+	private double wantedX;
+	private double wantedY; 
+	private double wantedAngle;
+	private double currentX;
+	private double currentY;
+	private double currentAngle;
+	
+	private double wantedStrafeVelocity;
+	private double wantedForwardVelocity; 
 	
 	
+	private double strafeError;
+	private double forwardError;
+	private double spinError;
+	
+	private PIDLoop strafeHandler;
+	private PIDLoop forwardHandler;
+	private PIDLoop spinHandler;
+	
+	public PathFollower() {
+		strafeHandler = new PIDLoop(0.0,0.0,0.0);
+		forwardHandler = new PIDLoop(0.0,0.0,0.0);
+		spinHandler = new PIDLoop(0.0,0.0,0.0);
+	}
+	
+	private void findTotalError() {
+		strafeError = wantedX - currentX; 
+		forwardError = wantedY - currentY;
+		spinError = wantedAngle - currentAngle;
+		
+	}
+	
+	private void setWantedCoords() {
+		wantedX = Trajectory.getWantedX();
+		wantedY = Trajectory.getWantedY(); 
+		wantedAngle = Trajectory.getWantedAngle();
+	}
 }
