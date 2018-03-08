fcda9307bc44024a2fd115500fad1a83e30db1cf
Jonathan Heidegger <jth3141@gmail.com>
2018-03-08 11:23:51 -0500

    worked in trajectory class

--- ProjectCRAB/src/Auton/Trajectory.java
+++ ProjectCRAB/src/Auton/Trajectory.java
@@ -1,5 +1,6 @@
 package Auton;
 
+import java.awt.geom.Arc2D;
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -7,6 +8,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class Trajectory {
 	private double kMaxVelocity;
 	private double kMaxAcceleration; 
+	
 	private double currentX;
 	private double currentY;
 	private double currentAngle;
@@ -16,9 +18,15 @@ public class Trajectory {
 	private double wantedAngle;
 	private double currentVelocity;
 	private double wantedVelocity;
+	private double timeStep = .02;
+	private double simTime = 0.0;
 	private ArrayList<Waypoint>  points;
+	private ArrayList<Double> speed;
+	private ArrayList<Double> angle;
 	public Trajectory() {
 		points = new ArrayList<Waypoint>();
+		speed = new ArrayList<Double>();
+		angle = new ArrayList<Double>();
 	}
 	public void addWaypoint(Waypoint w)
 	{
@@ -26,7 +34,19 @@ public class Trajectory {
 	}
 	public void calculateTrajectory()
 	{
-		
+		double currSpeed = 0.0;
+		double currX = points.get(0).getX();
+		double currY = points.get(0).getY();
+		int waypointIdx = 1;
+		while(simTime < 15.0)
+		{
+			if(currSpeed < kMaxVelocity)
+			{
+				
+			}
+			angle.add(Math.atan2(points.get(waypointIdx).getY()-currY, points.get(waypointIdx).getX()-currX));
+			simTime+= timeStep;
+		}
 	}
 	public void setWantedPos() {
 		
