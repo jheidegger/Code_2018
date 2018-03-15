0c6f62e28bc28aebe9d73c1dd416e61c8e737674
Jonathan Heidegger <jth3141@gmail.com>
2018-03-15 18:27:12 -0400

    Pneumatics class add
    Co-Authored-By: Harrison McCarty <harrisonmccarty301@gmail.com>

---
+++ ProjectCRAB/src/Subsystem/Pneumatics.java
@@ -0,0 +1,27 @@
+package Subsystem;
+
+public class Pneumatics extends Subsystem {
+
+	public void pressurize() {
+		
+	}
+	
+	@Override
+	public void zeroAllSensors() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkSystem() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void registerLoop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
